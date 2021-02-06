64c771d67a13af15470561233e6225d403892653
maca99 <matteo.cavasinni@hotmail.it>
2021-02-06 16:26:59 +0100

    ytgfty

--- apertura.java
+++ apertura.java
@@ -1,2 +1,5 @@
 public interface apertura {
+	//interfaccia che permette di aprire la serratura di una postazione dalla stazione
+	//E' implemetata dalle postazioni
+	boolean apriSerratura();
 }
--- consegnaBici.java
+++ consegnaBici.java
@@ -1,2 +1,5 @@
 public interface consegnaBici {
+	//E' implementata dalle stazioni
+	//
+	public boolean consegnaBici(Postazione postazione);
 }
