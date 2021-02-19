f3cc82b0c7a8f01db919c38d2a8dcccb5d6869c2
maca99 <43297703+maca99@users.noreply.github.com>
2021-02-19 02:18:21 +0100

    modifica gestione stazioni

--- bikesharing/src/sistemacentrale/Gestorestazioni.java
+++ bikesharing/src/sistemacentrale/Gestorestazioni.java
@@ -2,33 +2,60 @@ package sistemacentrale;
 
 import stazione.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Gestorestazioni {
+public class GestoreStazioni {
 
-	private Map<String, Stazione> StazioniMap;
+	private Map<Stazione,Integer> statoStazioni;
+	private static GestoreStazioni istance = new GestoreStazioni();
+	//serve per tenere traccia degli utenti che iniziano una nuova corsa prenotata
+	private List<Prenotazione> prenotazioni;
+	
+	private GestoreStazioni() {
+		//metodo che prende in input le stazioni
+		statoStazioni = new HashMap<Stazione,Integer>();
+	}
 
 	/**
 	 * 
 	 * @param prenotazione
 	 */
-	public void prenotazione(Prenotazione prenotazione) {
-		// TODO - implement Gestorestazioni.prenotazione
-		throw new UnsupportedOperationException();
+	public boolean prenotazione(Prenotazione prenotazione) {
+		/*gli viene passata una prenotazione e riserva un posto in una stazione
+		*all'utente della prenotazione*/
+		int libere = statoStazioni.get(prenotazione.getStazione());
+		if(libere > 0) {
+			statoStazioni.replace(prenotazione.getStazione(), libere--);
+			prenotazioni.add(prenotazione);
+			return true;
+		}
+		return false;
+	}
+	
+	public void cancellaPrenotazione(Prenotazione prenotazione) {
+		int libere = statoStazioni.get(prenotazione.getStazione());
+		statoStazioni.replace(prenotazione.getStazione(), libere++);
+		prenotazioni.remove(prenotazione);
 	}
 
 	/**
 	 * 
 	 * @param corsa
 	 */
-	public void nuovaCorsa(Corsa corsa) {
-		// TODO - implement Gestorestazioni.nuovaCorsa
+	public boolean nuovaCorsa(Corsa corsa) {
+		
+		
 		throw new UnsupportedOperationException();
 	}
 
-	public void getStatoStazioni() {
+	public List<Stazione> getStatoStazioni() {
 		// TODO - implement Gestorestazioni.getStatoStazioni
 		throw new UnsupportedOperationException();
+	}
+	public static GestoreStazioni getIstance() {
+		return istance;
 	}
 
 }
