8dc945411f3a5bd5cf5a24a7279ffba956295860
longobucco <lucavisconti2000@gmail.com>
2021-02-21 23:21:31 +0100

    Update code

--- bikesharing/src/sistemacentrale/ServiziUtente.java
+++ bikesharing/src/sistemacentrale/ServiziUtente.java
@@ -7,59 +7,58 @@ import java.util.List;
 
 import stazione.Stazione;
 
-public class ServiziUtente /* implements *****/  {
+public class ServiziUtente {
 
-    private Prenotazione prenotazione;
-    private Corsa corsa;
-    private Abbonamento abbonamento;
-    private Utente utente;
-    private Integer Strike;
-    private GestoreStazioni stazioni;
-    
-    public ServiziUtente(Utente utente) {
-        this.setUtente(utente);
-        prenotazione=null;
-        corsa=null;
-        stazioni = GestoreStazioni.getIstance();
-        //facciamo finta che venga preso da un DB
-        abbonamento = new Abbonamento();
-    }
+	private Prenotazione prenotazione;
+	private Corsa corsa;
+	private Abbonamento abbonamento;
+	private Utente utente;
+	private Integer Strike;
+	private GestoreStazioni stazioni;
 
-    private boolean verificaAbbonamento() {
-        return false;
+	public ServiziUtente(Utente utente) {
+		this.setUtente(utente);
+		prenotazione = null;
+		corsa = null;
+		stazioni = GestoreStazioni.getIstance();
+		abbonamento = new Abbonamento();
+	}
 
-    }
+	private boolean verificaAbbonamento() {
+		return false;
 
-    private boolean iniziaCorsa(String stazione) {
-        if(corsa != null) return false;
+	}
 
-        if(prenotazione != null) {
+	private boolean iniziaCorsa(String stazione) {
+		if (corsa != null)
+			return false;
 
-        }
-        return false;
+		if (prenotazione != null) {
 
+		}
+		return false;
 
-    }
+	}
 
-    public boolean prenotazione(String stazione) {
-        if(prenotazione != null)return false;
-        prenotazione = new Prenotazione();
-        prenotazione.setStazione(stazione);
-        stazioni.prenotazione(prenotazione);
-        return true;
-    }
-    
-    public List<Stazione> getAllStazioni(){
-    	return new LinkedList<Stazione>(stazioni.getStazioni());
-    }
+	public boolean prenotazione(String stazione) {
+		if (prenotazione != null)
+			return false;
+		prenotazione = new Prenotazione();
+		prenotazione.setStazione(stazione);
+		stazioni.prenotazione(prenotazione);
+		return true;
+	}
 
-    public Utente getUtente() {
-        return utente;
-    }
+	public List<Stazione> getAllStazioni() {
+		return new LinkedList<Stazione>(stazioni.getStazioni());
+	}
 
-    public void setUtente(Utente utente) {
-        this.utente = utente;
-    }
+	public Utente getUtente() {
+		return utente;
+	}
 
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
 
 }
